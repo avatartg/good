d4c83fceada026c58dc0022a5b4d28e10044d545
avatartg <avatarbot20@gmail.com>
2016-07-17 15:08:11 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -237,7 +237,7 @@ function create_config( )
     },
     sudo_users = {212295971,103649648,111020322,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v4
+    about_text = [[AVATAR GOOD v1.1
 An advanced administration bot based on TG-CLI written in Lua
 
 https://github.com/avatartg/good
